c051e58337d5b33d4a31273d5e626fa9040a4072
Juned mansuri <junedmansuri639@gmail.com>
2022-10-26 20:46:17 +0530

    first

---
+++ README.txt
@@ -0,0 +1,3 @@
+Client side chat applicatio
+ > To run this application istall java 8 or higher version.
+ > open in any ide compile and run it easily.
--- cilent.java
+++
@@ -1,8 +0,0 @@
-java.net.*;
-class client
-{
-	public static void main(String[] args){
-	        Socket s = new Socket("/localhost",8900);
-			System.out.println("request has sent");
-	}
-}
